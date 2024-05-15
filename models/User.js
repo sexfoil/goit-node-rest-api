968cb57784367852c5278e869dcc7f1becdb2ca0
Slava Poliakov <78slavapolyakov@gmail.com>
2024-05-15 21:44:46 +0300

    Add step 1

---
+++ models/User.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+import { handleSaveError, setUpdateSetings } from "./hooks.js";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.pre("findOneAndUpdate", setUpdateSetings);
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+userSchema.post("save", handleSaveError);
+
+const User = model("user", userSchema);
+
+export default User;
